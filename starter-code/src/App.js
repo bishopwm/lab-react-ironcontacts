72a9519146f354f1b0017f93314202a3bc789bc8
bishopwm <bishopwm@g.cofc.edu>
2020-04-21 12:54:56 -0400

    render list, first iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 class App extends Component {
+
+  state = {
+    actors: [...contacts].slice(0,5)
+  }
+
+  showActors = () => {
+    let actorsList = [];
+    for(let i = 0; i<this.state.actors.length; i++){
+      actorsList.push(this.state.actors[i].name)
+    }
+    return actorsList;
+  }
+
   render() {
+    console.log(this.state.actors[0].name);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <>{this.showActors()}</>
       </div>
+      
     );
   }
 }
